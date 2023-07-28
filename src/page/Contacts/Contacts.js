6601e13ff01d9379792fa4a0fb95bc6db8efb973
momen105 <momentohin02@gmail.com>
2023-07-28 12:08:46 +0600

    update

--- src/page/Contacts/Contacts.js
+++ src/page/Contacts/Contacts.js
@@ -4,36 +4,36 @@ import {FiMail} from 'react-icons/fi'
 import {FaLocationArrow,FaPhone,FaUserClock} from 'react-icons/fa'
 function Contacts() {
     return (
-        <div className='flex m-auto mt-8 h-[62vh] bg-transparent w-[70vw] border border-gray-800'>
-            <div className='w-1/2 border-r border-gray-800 flex flex-col justify-between'>
-                <div className='flex justify-between'>
-                    <div className='h-[150px] w-[200px] border-r border-b contacts-context'>
+        <div className='md:flex m-auto mt-8 h-[62vh] bg-transparent w-[70vw] border border-gray-800 overflow-y-auto'>
+            <div className='md:w-1/2 md:border-r md:border-gray-800 md:flex md:flex-col md:justify-between max-md:bg-body'>
+                <div className='flex md:justify-between max-md:flex-col max-md:items-center max-md:gap-2 max-md:pt-2'>
+                    <div className='h-[150px] w-[200px] md:border-r md:border-b max-md:border contacts-context'>
                         <p className='icon'><FiMail /></p>
                         <span className='text-sm'>
                             <p>momentohin02@gmail.com</p>
                             <p>momentohin105@gmail.com</p>
                         </span>
                     </div>
-                    <div className='h-[150px] w-[200px] border-l border-b contacts-context'>
+                    <div className='h-[150px] w-[200px] md:border-l md:border-b max-md:border contacts-context'>
                         <p className='icon'><FaLocationArrow /></p>
                         <p className='text-sm'>House:23, Road-3, Nikunja-2, Dhaka, Bangladesh</p>
                         </div>
                 </div>
-                <div className=' flex justify-between'>
-                    <div className='h-[150px] w-[200px] border-t border-r contacts-context'>
+                <div className='flex md:justify-between max-md:flex-col max-md:items-center max-md:gap-2 max-md:pt-2'>
+                    <div className='h-[150px] w-[200px] md:border-t md:border-r max-md:border contacts-context'>
                         <p className='icon'><FaPhone /></p>
                         <spna className="text-sm">
                             <p>+8801777305810</p>
                             <p>+8801515233688</p>
                         </spna>
                         </div>
-                    <div className='h-[150px] w-[200px] border-t border-l contacts-context'>
+                    <div className='h-[150px] w-[200px] md:border-t md:border-l max-md:border  contacts-context'>
                         <p className='icon'>< FaUserClock/> </p>
                         <p className='text-sm text-justify'>I am available for full-time/part-time job hire.</p>
                     </div>
                 </div>
             </div>
-            <div className='w-1/2 bg-body p-10'>
+            <div className='md:w-1/2 bg-body p-10 max-md:pt-5'>
                 <form>
                     <div className='name'>
                         <input type="text" id="name" name="name" className='input'></input>

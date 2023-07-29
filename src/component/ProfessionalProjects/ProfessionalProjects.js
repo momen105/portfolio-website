b32c999f9264a8c27f63bbb8baee3dc68119cf50
momen105 <momentohin02@gmail.com>
2023-07-29 10:32:18 +0600

    update

--- src/component/ProfessionalProjects/ProfessionalProjects.js
+++ src/component/ProfessionalProjects/ProfessionalProjects.js
@@ -13,70 +13,74 @@ function ProfessionalProjects() {
         <div className='content-animate flex h-[450px] border  border-gray-800 bg-body overflow-y-auto'>
             <div className='w-2/5'>
                 <div className='relative h-[40vh] p-10 border-b border-gray-800 overflow-hidden group transition-all'>
+                <div className='top-0 left-0 w-full h-0 bg-primary group-hover:h-4 transition-all duration-500'>
+                </div>
                     <img className='w-full h-full filter brightness-[0.2]' src={school}></img>
                     <div className='absolute max-w-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
                         <p className='text-lg text-[#4bffa5]'>School Management System</p>
                     </div>
-                    <div className='absolute -left-full -bottom-full h-full w-full bg-primary group-hover:bottom-0 group-hover:left-0 transition-all duration-1000'>
-                        
-                    </div>
                 </div>
+
                 <div className='relative h-[40vh] p-10 border-b border-gray-800 overflow-hidden group transition-all'>
+                <div className='top-0 left-0 w-full h-0 bg-primary group-hover:h-4 transition-all duration-500'>
+
+                </div>
                     <img className='w-full h-full filter brightness-[0.2]' src={door}></img>
                     <div className='absolute max-w-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
                         <p className='text-lg text-[#4bffa5]'>Automated Door Open System</p>
                     </div>
-                    <div className='absolute -left-full -bottom-full h-full w-full bg-primary group-hover:bottom-0 group-hover:left-0 transition-all duration-1000'>
-                        
-                        </div>
+
                 </div>
-                <div className='relative h-[40vh] p-10 border-b border-gray-800 overflow-hidden group transition-all'>
+                <div className='relative h-[40vh] p-10 border-b border-gray-800 overflow-hidden group transition-all'>                
+                    <div className='top-0 left-0 w-full h-0 bg-primary group-hover:h-4 transition-all duration-500'>
+
+                    </div>
                     <img className='w-full h-full filter brightness-[0.2]' src={restro}></img>
                     <div className='absolute max-w-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
                         <p className='text-lg text-[#4bffa5]'>Restaurant Website</p>
                     </div>
-                    <div className='absolute -left-full -bottom-full h-full w-full bg-primary group-hover:bottom-0 group-hover:left-0 transition-all duration-1000'>
-                        
-                        </div>
                 </div>
                 <div className='relative h-[40vh] p-10 border-b border-gray-800 overflow-hidden group transition-all'>
+                <div className='top-0 left-0 w-full h-0 bg-primary group-hover:h-4 transition-all duration-500'>
+
+                    </div>
                     <img className='w-full h-full filter brightness-[0.2]' src={office}></img>
                     <div className='absolute max-w-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
                         <p className='text-lg text-[#4bffa5]'>Corporate Website</p>
                     </div>
-                    <div className='absolute -left-full -bottom-full h-full w-full bg-primary group-hover:bottom-0 group-hover:left-0 transition-all duration-1000'>
-                        
-                        </div>
+                
                 </div>
             </div>
             <div className='w-3/5'>
                 <div className='relative p-10 border-b border-l border-gray-800 overflow-hidden group transition-all'>
+                <div className='top-0 left-0 w-full h-0 bg-primary group-hover:h-4 transition-all duration-500'>
+
+                    </div>
                     <img className='h-[50vh] w-full filter brightness-[0.2]' src={hrm}></img>
                     <div className='absolute max-w-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
                         <p className='text-lg text-[#4bffa5]'>Human Resource Management One</p>
                     </div>
-                    <div className='absolute -left-full -bottom-full h-full w-full bg-primary group-hover:bottom-0 group-hover:left-0 transition-all duration-1000'>
-                        
-                        </div>
+
                 </div>
                 <div className='relative p-10 border-b border-l border-gray-800 overflow-hidden group transition-all'>
+                    <div className='top-0 left-0 w-full h-0 bg-primary group-hover:h-4 transition-all duration-500'>
+
+                    </div>
                     <img className='h-[50vh] w-full filter brightness-[0.2]' src={hrm2}></img>
                     <div className='absolute max-w-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
                         <p className='text-lg text-[#4bffa5]'>Human Resource Management Two</p>
                     </div>
-                    <div className='absolute -left-full -bottom-full h-full w-full bg-primary group-hover:bottom-0 group-hover:left-0 transition-all duration-1000'>
-                        
-                        </div>
                 </div>
 
                 <div className='relative p-10 border-l border-gray-800 overflow-hidden group transition-all'>
+                    <div className='top-0 left-0 w-full h-0 bg-primary group-hover:h-4 transition-all'>
+
+                </div>
                     <img className='h-[50vh] w-full filter brightness-[0.2]' src={menu}></img>
                     <div className='absolute max-w-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
                         <p className='text-lg text-[#4bffa5]'>Digital Menu</p>
                     </div>
-                    <div className='absolute -left-full -bottom-full h-full w-full bg-primary group-hover:bottom-0 group-hover:left-0 transition-all duration-1000'>
-                        
-                        </div>
+                    
                 </div>
 
             </div>
